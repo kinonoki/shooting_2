5844e0ccecb9a8a3c0411c0896ac8e6c94a590fe
Daiwa Kobayashi <hw16a078@oecu.jp>
2017-11-28 15:13:29 +0900

    cを実装しました。

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -3,8 +3,8 @@
 
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
+// TODO: 砲台を青い壁に沿って上下に動かす。(C) clear
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D) clear
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
@@ -16,6 +16,7 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
+int     pos = 1;
 
 
 // ゲーム開始時に呼ばれる関数です。
@@ -26,6 +27,7 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -35,38 +37,49 @@ void Update()
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
     }
-
+    
     // 弾の移動
     if (bulletPos.x > -999) {
-        bulletPos.x += 10 * Time::deltaTime;
-
+        bulletPos.x += 100 * Time::deltaTime;
+        
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
+        
         if (targetRect.Overlaps(bulletRect)) {
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
-
+    
     // 背景の描画
     Clear(Color::cyan);
     FillRect(Rect(-320, -240, 640, 100), Color::green);
-
+    
     // 雲の描画
     DrawImage("cloud1.png", cloudPos);
-
+    
     // 弾の描画
     if (bulletPos.x > -999) {
         DrawImage("bullet.png", bulletPos);
     }
-
+    
     // 砲台の描画
     FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
     DrawImage("cannon.png", cannonPos);
-
+    
+    //ここ小林
+    cannonPos.y += pos;
+    if(cannonPos.y == -50){
+        pos *= -1;
+    }
+    if (cannonPos.y == -150) {
+        pos *= -1;
+    }
+    
+    
     // ターゲットの描画
     FillRect(targetRect, Color::red);
-
+    
     // スコアの描画
     SetFont("nicoca_v1.ttf", 20.0f);
     DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
